531a8a3d40339f9ff0695f0506747600ff574900
Grady Rogers <rogersgt819@gmail.com>
2017-10-15 23:16:53 -0400

    v2

--- src/app.js
+++ src/app.js
@@ -3,32 +3,42 @@ import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
 import attachFastClick from 'fastclick';
 import Element from 'element-ui'
-import vide from 'vide';
-import $ from 'jquery';
- 
-import home from './home/home.vue';
+import home from './components/home.vue';
+import contact from './components/contact.vue';
+import shows from './components/shows.vue';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(Element);
-Vue.use(vide);
-Vue.use(() => { window.$ = window.jQuery = $; });
 
 const routes = [
   {
     path: '/',
     component: home,
     name: 'home'
+   },
+   {
+     path: '/contact',
+     component: contact,
+     name: 'contact'
+   },
+   {
+    path: '/shows',
+    component: shows,
+    name: 'shows'
+  },
+   {
+     path: '*',
+     redirect: '/'
    }
 ];
 
 
 const router = new VueRouter({
   routes,
-  history: true
+  history: true,
+  hashband: false
 });
-
-router.push('/');
 
 const app = new Vue({
   router
---
+++ src/components/contact.vue
@@ -0,0 +1,41 @@
+<template>
+<section class="outerSection">
+<nav-menu></nav-menu>
+
+<div class="innerSection">
+  <form class="contactForm">
+    <h1 class="title">Contact Badweather</h1>
+    <p class="center">
+      Thanks for reaching out! We'll do our best to respond withing 24 hours.
+    </p>
+    <input type="text" placeholder="email address" />
+    <input type="text" placeholder="confirm email address" />
+    <input type="text" placeholder="subject (i.e. Booking)" />
+    <textarea type="text" placeholder="message" />
+    <button v-on:click="submit">Submit</button>
+  </form>
+</div>
+</section>
+</template>
+<script>
+import navMenu from './navMenu.vue';
+
+export default {
+  components: {
+    navMenu: navMenu
+  },
+  data: function() {
+    return {
+      email: null,
+      confirmEmail: null,
+      subject: null,
+      message: null
+    };
+  },
+  methods: {
+    submit: function() {
+      alert('Sorry for the inconvenience! Our site is currently under construction...');
+    }
+  }
+}
+</script>
